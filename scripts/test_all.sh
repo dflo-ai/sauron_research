0ee6c9325a66f4ea44ccee553291f4984533de3a
miniHaLe <thanhha.le2323@gmail.com>
2026-02-04 16:52:50 +0700

    feat(scripts): add test runner script

---
+++ scripts/test_all.sh
@@ -0,0 +1,4 @@
+python scripts/demo_video_reid_inference.py --video data/videos/demo_k1.mp4 --output outputs/ --minutes 1
+python scripts/demo_video_reid_inference.py --video data/videos/demo.mp4 --output outputs/
+python scripts/demo_video_reid_inference.py --video data/videos/demo2.mp4 --output outputs/
+python scripts/demo_video_reid_inference.py --video data/videos/cctv.mp4 --output outputs/
